9ed6d2a4215b57be8c85919cb96a1f787f82bdde
Diana García <diana.eli.gg@gmail.com>
2019-08-10 18:12:33 -0500

    Moved things to  Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Cohort from "./pages/Cohort"
 import Login from "./pages/Login"
 import CohortList from "./pages/CohortList"
 import {
-  Container, Menu
+  Container
 } from 'semantic-ui-react'
 import {BCS_TOKEN} from "./localKeys"
-import Emoji from "./components/Emoji"
+
 import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
 
 class App extends Component {
   state = {
@@ -20,39 +21,12 @@ class App extends Component {
 
   logOut = () => {
     localStorage.removeItem(BCS_TOKEN);
-    console.log(this.props.router);
-    // this.props.history.push('/login');
   }
 
   render() {
     return (
       <Router>
-        <Menu fixed='top' inverted>
-        <Menu.Item color='green'>
-        <Emoji label="sheep" symbol="🐸"/> BCS-MISSING-VIDEOS
-        </Menu.Item>
-
-        <Route
-                path="/cohort"
-                render={ props => (
-                    <Menu.Item as={Link} to='/cohortList'
-                      name='CohortList'
-                    >
-                      Cohort List
-                    </Menu.Item>
-                )}
-              />
-        
-        <Menu.Menu position='right'>
-          <Menu.Item
-            name='LogOut'
-            as={Link} to="/login"
-            onClick={this.logOut}
-          >
-            Log Out
-          </Menu.Item>
-        </Menu.Menu>
-      </Menu>
+        <NavBar logOut={this.logOut}></NavBar>
       <Container style={{ marginTop: '6em'}}>
       <div>
           <Route exact path="/" component={Login} />
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Emoji from "./Emoji";
+import {Menu} from 'semantic-ui-react';
+import { Link, Route } from "react-router-dom";
+
+const NavBar = props => {
+    return (
+        <Menu fixed='top' inverted>
+            <Menu.Item color='green'>
+            <Emoji label="sheep" symbol="🐸"/> BCS-MISSING-VIDEOS
+            </Menu.Item>
+
+            <Route
+                    path="/cohort"
+                    render={ props => (
+                        <Menu.Item as={Link} to='/cohortList'
+                        name='CohortList'
+                        >
+                        Cohort List
+                        </Menu.Item>
+                    )}
+                />
+            
+            <Menu.Menu position='right'>
+            <Menu.Item
+                name='LogOut'
+                as={Link} to="/login"
+                onClick={props.logOut}
+            >
+                Log Out
+            </Menu.Item>
+            </Menu.Menu>
+        </Menu>
+    )
+}
+export default NavBar;
