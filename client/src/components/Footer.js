3579bbb75a96d62e0203bda408680741e0fbd567
Diana García <diana.eli.gg@gmail.com>
2020-06-04 19:51:24 -0500

    added github code link

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,6 +6,10 @@ const footerStyle ={
     fontSize: "10px"
 }
 
+const githubLink = {
+    color: "black"
+}
+
 const Footer = () => {
     //totally legit obfuscation method
     let abcd = "diana";
@@ -16,6 +20,13 @@ const Footer = () => {
     abcd = abcd + ".com";
     return(
         <div style={footerStyle}>
+            <div>
+                <a  style={githubLink} href="https://github.com/dianaeligg/bcs-missing-videos" target="_blank"
+                    rel="noopener noreferrer">
+                    <img src="./images/GitHub-Mark-32px.png" alt="Github logo" />
+                    <span>See the code</span>
+                </a> 
+            </div>
             <div>Found a bug? Have a suggestion on how this app can be improved?</div>
             <div>Send an e-mail to: 
                 <a href={`mailto:${abcd}?subject=Comment about BCS Missing Videos`} target="_blank" rel="noopener noreferrer"> {abcd}</a>
